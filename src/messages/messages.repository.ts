b1ca084a5d033f98a9b8cfa23d99d5ebc96c36bc
Minh Giang <minhgiang241@gmail.com>
2024-07-25 17:35:50 +0700

    fix message

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -4,19 +4,19 @@ import { readFile, writeFile } from 'fs/promises';
 @Injectable()
 export class MessagesRepository {
   async findOne(id: string) {
-    const contents = await readFile('message.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages[id];
   }
 
   async findAll() {
-    const contents = await readFile('message.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages;
   }
 
   async create(message: string) {
-    const contents = await readFile('message.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
 
     const id = Math.floor(Math.random() * 999);
